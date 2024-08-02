2607f06a82d2d5677217457fed598f218259aa5f
D0915666 <lovewinds090609@gmail.com>
2024-08-02 09:58:20 +0800

    first commit - connect sql - use boostrap to change css - add Category Models&Controller

---
+++ BookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using BookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        /// <summary>
+        /// 創建名為Categories資料表 資料格式為Models中的Category
+        /// </summary>
+        public DbSet<Category> Categories { get; set; }
+        /// <summary>
+        /// 新增資料
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ///新增3筆資料 動作 科幻 歷史
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+
+    }
+}
