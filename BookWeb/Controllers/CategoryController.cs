5311fbb39e583e6303577af0e7ceb9a841caff35
D0915666 <lovewinds090609@gmail.com>
2024-08-03 09:47:08 +0800

    implement edit category

--- BookWeb/Controllers/CategoryController.cs
+++ BookWeb/Controllers/CategoryController.cs
@@ -35,5 +35,35 @@ namespace BookWeb.Controllers
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            ///主鍵不允許null而且從1開始 防止查詢&操作無效資料
+            if (id == null || id == 0) { 
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id); //用主鍵找實體
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id); //不限定主鍵
+            //Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault(); //複雜查詢可使用
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Order不能跟名稱一樣");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
