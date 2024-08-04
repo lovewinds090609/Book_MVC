fa869137ee96829f26e9e1cdf0a37eea8fa10ca9
D0915666 <lovewinds090609@gmail.com>
2024-08-05 02:02:48 +0800

    修改顯示語言

--- Book.Models/Category.cs
+++ Book.Models/Category.cs
@@ -9,10 +9,10 @@ namespace Book.Models
         public int Id { get; set; }
         [Required(ErrorMessage ="名稱不能為空白")]
         [MaxLength(30,ErrorMessage ="名稱長度不能大於30個字元")]
-        [DisplayName("Category Name")]
+        [DisplayName("類別名稱")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "編號不能為空白")]
-        [DisplayName("Display Order")]
+        [Required(ErrorMessage = "顯示順序不能為空白")]
+        [DisplayName("顯示順序")]
         [Range(1,100,ErrorMessage ="範圍必須在1到100之間")]
         public int DisplayOrder { get; set; }
     }
