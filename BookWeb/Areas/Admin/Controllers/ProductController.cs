3ff39ac0808fef3723529a2d32e3d1a655aaa7e6
D0915666 <lovewinds090609@gmail.com>
2024-08-04 19:05:57 +0800

    implement product crud

--- BookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -7,11 +7,100 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BookWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class ProductController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
+            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            return View(objProductList);
+        }
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("Name", "Order不能跟名稱一樣");
+            //}
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.save();
+                TempData["success"] = "創建成功";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            ///主鍵不允許null而且從1開始 防止查詢&操作無效資料
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //Category? categoryFromDb = _db.Categories.Find(id); //用主鍵找實體
+            Product? ProductFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id); //不限定主鍵
+            //Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault(); //複雜查詢可使用
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ProductFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("Name", "Order不能跟名稱一樣");
+            //}
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.save();
+                TempData["success"] = "修改成功";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            ///主鍵不允許null而且從1開始 防止查詢&操作無效資料
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? ProductFromDb = _unitOfWork.Product.Get(u => u.Id == id); //用主鍵找實體
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id); //不限定主鍵
+            //Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault(); //複雜查詢可使用
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ProductFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.save();
+            TempData["success"] = "刪除成功";
+            return RedirectToAction("Index");
         }
     }
 }
